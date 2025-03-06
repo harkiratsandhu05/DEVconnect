072d03c9091e37e28e9bbb2befc59c963c5bd781
harkiratsandhu05 <164857671+harkiratsandhu05@users.noreply.github.com>
2025-03-05 18:53:42 -0800

    Delete DevConnect-main-2 directory

--- DevConnect-main-2/client/src/pages/LoginPage.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "../components/form/LoginForm";
-
-export default function LoginPage() {
-    return (
-        <div className="max-w-md mx-auto">
-            <LoginForm />
-        </div>
-    );
-}
--- DevConnect-main-2/client/src/pages/RegisterPage.tsx
+++
@@ -1,9 +0,0 @@
-import RegisterForm from "../components/form/RegisterForm";
-
-export default function RegisterPage() {
-    return (
-        <div className="max-w-md mx-auto">
-            <RegisterForm />
-        </div>
-    );
-}
